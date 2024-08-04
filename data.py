85ea566a0889c837ceb1e352208b320782fc9716
Akshat Khurana <akshat.khurana@yahoo.com>
2024-08-04 22:27:08 +1000

    Setup Tkinter stuff
    This is my first time using Custom Tkinter so had to look up a bunch of tutorials and documentation. Still waiting on Etsy API key approval.

--- data.py
+++ data.py
@@ -4,4 +4,17 @@ probability of them being sold (based off reviews and other sale metrics) in dat
 """
 
 import requests
-import pillow
+import JSON
+from PIL import *
+
+URL = ""
+API_KEY = ""
+
+req = requests.get(URL)
+print(req.status_code)
+
+image_urls = None
+
+for image in image_urls:
+    with open('image_name.jpg', 'wb') as f:
+        f.write()
--- main.py
+++ main.py
@@ -3,10 +3,64 @@ The main file to run this application and provide a GUI which uses the predictio
 """
 
 import customtkinter as ctk
+from PIL import ImageTk, Image
+import os
+
+WIDTH = 800
+HEIGHT = 600
+
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("blue")
 
-root = ctk.CTk()
+class EtsyApp(ctk.CTk):
 
-root.title("Will it sell?")
-root.geometry('1000x600')
+    def __init__(self):
+        super().__init__()
+
+        self.resizable(False, False)
+        self.title("Etsy Sales Predictor")
+        self.geometry(f"{WIDTH}x{HEIGHT}")  
+
+        self.upload_button = ctk.CTkButton(self, 
+                                      text="Upload Design",
+                                      command=self.on_upload_click,
+                                      height=20,
+                                      width=100,
+                                      corner_radius=5,
+                                      font=("Tahoma", 20),
+                                      state="normal")
+        
+        self.upload_button.pack(pady=60, padx=60)  
+
+        self.chosen_image = None
+
+        self.chosen_label = ctk.CTkLabel(self, 
+                                              text="")
+        self.chosen_label.pack(pady=80)
+        
+        self.allow_to_go_ahead = False
+    
+    def on_upload_click(self):
+        allowed_file_types = [
+            ("Image files", ".png .jpg .jpeg")
+        ]
+
+        image_path = ctk.filedialog.askopenfilename(filetypes=
+                                                    allowed_file_types)
+    
+        if len(image_path) > 0:
+            img = Image.open(image_path)
+            self.chosen_image = ImageTk.PhotoImage(img)
+
+            image_path_to_display = image_path.split("/")[-1]
+            self.chosen_label.configure(text=
+                                        image_path_to_display)
+            
+            self.allow_to_go_ahead = True
+        else:
+            self.allow_to_go_ahead = False
+            self.chosen_label.configure(text=
+                                        "No file chosen.")
+
+app = EtsyApp()
+app.mainloop()
